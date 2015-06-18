6733e289d4cd57010c2b95a1974e2caf78f7661e
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-06-18 16:32:40 +0200

    remake of menu with new unity ui features

---
+++ Assets/scripts/Menus/StartMenu.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public Canvas playerOneCanvas;
+	public Canvas playerTwoCanvas;
+	public Canvas optionsCanvas;
+	public Canvas creditsCanvas;
+	public Slider volumeSlider;
+	private Canvas startCanvas;
+	private Canvas activeCanvas;
+	private float volSliderValue = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+		playerOneCanvas = playerOneCanvas.GetComponent<Canvas>();
+		playerTwoCanvas = playerTwoCanvas.GetComponent<Canvas>();
+		optionsCanvas = optionsCanvas.GetComponent<Canvas>();
+		creditsCanvas = creditsCanvas.GetComponent<Canvas>();
+
+		playerOneCanvas.enabled = false;
+		playerTwoCanvas.enabled = false;
+		optionsCanvas.enabled = false;
+		creditsCanvas.enabled = false;
+
+		startCanvas = GetComponent<Canvas>();
+		activeCanvas = startCanvas;
+		activeCanvas.enabled = true;
+
+		volumeSlider.value = volSliderValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera.main.GetComponent<AudioSource>().volume = volSliderValue;
+	}
+
+	void OnGUI() {
+		volSliderValue = volumeSlider.value;
+	}
+
+	public void OnQuitPressed() {
+		Application.Quit();
+	}
+
+	public void OnStartPressed() {
+		switchCanvas(playerOneCanvas);
+	}
+
+	public void OnNextPressed() {
+		// add player to GameProperties
+		switchCanvas(playerTwoCanvas);
+
+	}
+
+	public void OnPlayPressed() {
+		Application.LoadLevel(1);
+	}
+
+	private void switchCanvas(Canvas newCanvas) {
+		activeCanvas.enabled = false;
+		activeCanvas = newCanvas;
+		activeCanvas.enabled = true;
+	}
+
+	public void OnOptionsPressed() {
+		switchCanvas(optionsCanvas);
+	}
+
+	public void OnBackPressed() {
+		switchCanvas(startCanvas);
+	}
+	
+	public void OnCreditsPressed() {
+		switchCanvas(creditsCanvas);
+	}
+	
+
+}
---
+++ Assets/scripts/PlayerModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerModel {
+	private string _name;
+	private int _race;
+
+	public PlayerModel(string name, int race) {
+		_name = name;
+		_race = race;
+	}
+
+	public string getName() {
+		return _name;
+	}
+
+	public int getRace() {
+		return _race;
+	}
+}
