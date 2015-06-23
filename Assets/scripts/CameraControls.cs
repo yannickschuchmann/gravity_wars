6ac57d4c297472218834ffa37a6fa17df9761ea3
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-06-23 08:42:37 +0200

    added camera zoom on mouse scrollwheel

---
+++ Assets/scripts/CameraControls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Camera>().orthographicSize += Input.GetAxis("Mouse ScrollWheel");
+	}
+}
